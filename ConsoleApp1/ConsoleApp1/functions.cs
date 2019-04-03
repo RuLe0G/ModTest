a8ba344c19d9e0960a2dc6931de47164af8fe95b
RuLe0G <khlebnikov2015@yandex.ru>
2019-04-03 09:39:17 +0700

    F

---
+++ ConsoleApp1/ConsoleApp1/functions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class functions
+    {
+
+        public double cnv(double d)
+        {
+            if (d <= 0) throw new Exception("Значения должны быть > 0");
+            return (d * 2.54);
+        }
+
+        public bool chet(int n)
+        {
+            return (n % 2 == 0) ? true : false;
+        }
+
+        public int masTest(int[] mas)
+        {
+            if (mas.Length == 0) throw new Exception("Массив не должен быть пустым");
+            int max = mas[0];
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    if (max < mas[i])
+                        max = mas[i];
+                }
+            
+          
+
+
+            return max;
+
+        }
+
+
+
+
+        public int Ost(int l, int m)
+        {
+            if (m <= 0) throw new Exception("Значения должны быть > 0");
+
+            return (l % m);
+        }
+
+    }
+}
+
+
+
---
+++ ConsoleApp1/ConsoleApp1/testing_test.cs
@@ -0,0 +1,73 @@
+﻿//подключение NUnit
+using ConsoleApp1;
+using NUnit.Framework;
+using System;
+
+namespace testing_test
+{
+
+    [TestFixture]
+    class tasteCase
+    {
+        [TestCase]
+        public void Inch()
+        {
+            functions f = new functions();
+
+
+            Assert.AreEqual(12.7, f.cnv(5));
+
+            var ex = Assert.Throws<Exception>(() => f.cnv(-5));
+            Assert.That(ex.Message, Is.EqualTo("Значения должны быть > 0"));
+
+           Assert.AreEqual(2.54 * double.MaxValue, f.cnv(double.MaxValue));
+
+        }
+
+        [TestCase]
+        public void Chet()
+        {
+            //создание объекта, содержащего функцию
+            functions f = new functions();
+            //проверка возвращаемого значения, в первом случае оно должно быть истинно
+            //во втором ложно
+            Assert.IsTrue(f.chet(4));
+            Assert.IsFalse(f.chet(5));
+        }
+
+        [TestCase]
+        public void Mas()
+        {
+            int[] mas = new int[5] {1,2,3,4,5};
+            int[] mas1 = new int[5] { -1, -2, -3, -4, -5 };
+            int[] mas2 = new int[] {  };
+
+            functions f = new functions();
+
+            var ex = Assert.Throws<Exception>(() =>  f.masTest(mas2));
+
+            Assert.That(ex.Message, Is.EqualTo("Массив не должен быть пустым"));
+
+            Assert.DoesNotThrow(() => f.masTest(mas));
+            Assert.AreEqual(5, f.masTest(mas));
+            Assert.AreEqual(-1, f.masTest(mas1));
+
+
+        }
+
+        [TestCase]
+        public void Ost()
+        {
+
+            functions f = new functions();
+
+            var ex = Assert.Throws<Exception>(() => f.Ost(5, 0));
+
+            Assert.That(ex.Message, Is.EqualTo("Значения должны быть > 0"));
+
+            Assert.DoesNotThrow(() => f.Ost(5, 2));
+            
+        }
+    }
+
+}
